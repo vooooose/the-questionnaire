efc320b0a6a0d7ec246d9103bedf21091fbb87ed
PolinaShneider <school@codegirl.ru>
2022-10-09 03:17:30 +0200

    Add task

---
+++ script.js
@@ -0,0 +1,8 @@
+const form = document.querySelector(".form");
+form.addEventListener("submit", (event) => {
+  // Предотвращает действие браузера по умолчанию. В данном случае — отправку формы
+  // https://learn.javascript.ru/default-browser-action
+  event.preventDefault();
+
+  // Здесь твой код
+});
